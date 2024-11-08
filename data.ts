e67e3ed59ca68ff494b25615e1b2ac638b098ced
ejim favour <favourejim56@gmail.com>
2024-11-08 17:07:37 +0100

    added the ticket hive and note taker data

--- data.ts
+++ data.ts
@@ -31,26 +31,38 @@ import audiophile5 from "./assets/audiophile-5.png";
 import audiophile6 from "./assets/audiophile-6.png";
 import audiophile7 from "./assets/audiophile-7.png";
 import audiophile8 from "./assets/audiophile-8.png";
+import ticketHive1 from "./assets/tickethive-1.png";
+import ticketHive2 from "./assets/tickethive-2.png";
+import ticketHive3 from "./assets/tickethive-3.png";
+import ticketHive4 from "./assets/tickethive-4.png";
+import ticketHive5 from "./assets/tickethive-5.png";
+import notetaker1 from "./assets/notetaker-1.png";
+import notetaker2 from "./assets/notetaker-2.png";
+import notetaker3 from "./assets/notetaker-3.png";
+import notetaker4 from "./assets/notetaker-4.png";
+import notetaker5 from "./assets/notetaker-5.png";
+import notetaker6 from "./assets/notetaker-6.png";
 
 const ProjectsData = [
   {
     id: "1",
     title: "Brackifi",
     text: "Brackifi is a hedge fund that manages the assets of investors ensuring growth in investment.",
-    techs: ["Next Js", "Typescript", "Tailwind Css", "Node Js"],
+    techs: ["Next Js", "Typescript", "Tailwind Css", "Node Js", "Mongo db"],
     visit: "https://www.brackifi.io/",
     source: "",
     imgs: [brackifi1, brackifi2, brackifi3, brackifi4],
   },
   {
     id: "2",
-    title: "Techan Web App",
-    text: "Techan is a start up company with its main product 'PAPERLX'. It's aim is to make credentials and cerificates of individuals stored on the blockchain for easy accessibilty and verification",
-    techs: ["React Js", "Tailwind Css", "Context-api", "Firebase"],
-    visit: "https://techan.io/",
+    title: "Ticket Hive",
+    text: "Ticket hive is a ticketting company that provides a beautiful platform for people to get tickets to their favorite events. ",
+    techs: ["Typescript", "Next js", "Tailwind Css", "Nest js", "Postgresql"],
+    visit: "https://ticket-hive-clone.vercel.app/",
     source: "",
-    imgs: [techanImg1, techanImg2, techanImg3, techanImg4, techanImg5],
+    imgs: [ticketHive1, ticketHive2, ticketHive3, ticketHive4, ticketHive5],
   },
+
   {
     id: "3",
     title: "Blockview",
@@ -77,6 +89,23 @@ const ProjectsData = [
   },
   {
     id: "5",
+    title: "Note taker",
+    text: "Note taker is a simple note taking app that stores your notes per day and in different custom categories.",
+    techs: ["Flutter", "Riverpod"],
+    visit:
+      "https://drive.google.com/drive/folders/14ucSTPU16q01iskJt4xWahWBn3KgEn0K?usp=drive_link",
+    source: "https://github.com/ejim11/note-taker",
+    imgs: [
+      notetaker1,
+      notetaker2,
+      notetaker3,
+      notetaker4,
+      notetaker5,
+      notetaker6,
+    ],
+  },
+  {
+    id: "6",
     title: "Xwager",
     text: "Xwager is an app built for football fantasy fans. Users can wager against each other in pairs or groups",
     techs: ["Flutter", "Riverpod", "Laravel"],
@@ -86,7 +115,7 @@ const ProjectsData = [
   },
 
   {
-    id: "8",
+    id: "7",
     title: "Audiophile",
     text: " An e-commerce website for audio devices like headphones, speakers and earphones. Users can register and login or buy products as visitors",
     techs: ["React Js", "Scss", "Reduxjs/toolkit"],
@@ -102,6 +131,15 @@ const ProjectsData = [
       audiophile7,
       audiophile8,
     ],
+  },
+  {
+    id: "8",
+    title: "Techan Web App",
+    text: "Techan is a start up company with its main product 'PAPERLX'. It's aim is to make credentials and cerificates of individuals stored on the blockchain for easy accessibilty and verification",
+    techs: ["React Js", "Tailwind Css", "Context-api", "Firebase"],
+    visit: "https://techan.io/",
+    source: "",
+    imgs: [techanImg1, techanImg2, techanImg3, techanImg4, techanImg5],
   },
   //   {
   //     id: "3",
