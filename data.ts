b20bfb7d40a74e676f0105bd2e09d96257505134
ejim11 <favourejim56@gmail.com>
2024-12-14 19:28:23 +0100

    🎨 added qrest to project list

--- data.ts
+++ data.ts
@@ -42,8 +42,21 @@ import notetaker3 from "./assets/notetaker-3.png";
 import notetaker4 from "./assets/notetaker-4.png";
 import notetaker5 from "./assets/notetaker-5.png";
 import notetaker6 from "./assets/notetaker-6.png";
+import qrest1 from "./assets/qrest-1.png";
+import qrest2 from "./assets/qrest-2.png";
+import qrest3 from "./assets/qrest-3.png";
+import qrest4 from "./assets/qrest-4.png";
 
 const ProjectsData = [
+  {
+    id: "0",
+    title: "Qrest",
+    text: "Qrest software development company website",
+    techs: ["Next Js", "Typescript", "Tailwind Css"],
+    visit: "https://qrest-qtnj.vercel.app/",
+    source: "https://github.com/ejim11/qrest",
+    imgs: [qrest1, qrest2, qrest3, qrest4],
+  },
   {
     id: "1",
     title: "Brackifi",
@@ -62,7 +75,24 @@ const ProjectsData = [
     source: "",
     imgs: [ticketHive1, ticketHive2, ticketHive3, ticketHive4, ticketHive5],
   },
-
+  {
+    id: "4",
+    title: "Spotlx",
+    text: "Spotlx is a laundry app in Enugu, Nigeria. Users can order for different laundry services from anywhere in the enugu city.",
+    techs: ["React Native", "Reduxjs/toolkit", "Firebase"],
+    visit: "https://play.google.com/store/apps/details?id=com.akayudeh.Spotlx",
+    source: "",
+    imgs: [spotlx1, spotlx2, spotlx3, spotlx4, spotlx5, spotlx6, spotlx7],
+  },
+  {
+    id: "8",
+    title: "Techan Web App",
+    text: "Techan is a start up company with its main product 'PAPERLX'. It's aim is to make credentials and cerificates of individuals stored on the blockchain for easy accessibilty and verification",
+    techs: ["React Js", "Tailwind Css", "Context-api", "Firebase"],
+    visit: "https://techan.io/",
+    source: "",
+    imgs: [techanImg1, techanImg2, techanImg3, techanImg4, techanImg5],
+  },
   {
     id: "3",
     title: "Blockview",
@@ -78,15 +108,7 @@ const ProjectsData = [
     source: "https://github.com/ejim11/Blockview",
     imgs: [blockview1, blockview2, blockview3],
   },
-  {
-    id: "4",
-    title: "Spotlx",
-    text: "Spotlx is a laundry app in Enugu, Nigeria. Users can order for different laundry services from anywhere in the enugu city.",
-    techs: ["React Native", "Reduxjs/toolkit", "Firebase"],
-    visit: "",
-    source: "",
-    imgs: [spotlx1, spotlx2, spotlx3, spotlx4, spotlx5, spotlx6, spotlx7],
-  },
+
   {
     id: "5",
     title: "Note taker",
@@ -132,15 +154,7 @@ const ProjectsData = [
       audiophile8,
     ],
   },
-  {
-    id: "8",
-    title: "Techan Web App",
-    text: "Techan is a start up company with its main product 'PAPERLX'. It's aim is to make credentials and cerificates of individuals stored on the blockchain for easy accessibilty and verification",
-    techs: ["React Js", "Tailwind Css", "Context-api", "Firebase"],
-    visit: "https://techan.io/",
-    source: "",
-    imgs: [techanImg1, techanImg2, techanImg3, techanImg4, techanImg5],
-  },
+
   //   {
   //     id: "3",
   //     title: "Escrow Dapp",
