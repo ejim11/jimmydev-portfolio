62238aec146dfcd2fc406ccae7e75e9b4f4da362
ejim11 <favourejim56@gmail.com>
2025-05-15 17:41:23 +0100

    ✅ updated the cv link

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -46,7 +46,7 @@ const AboutMe = () => {
         >
           <a
             href={
-              "https://drive.google.com/file/d/14qcCADzQpXvY9WECHi3g3gxQM5Wm0oRv/view?usp=sharing"
+              "https://drive.google.com/file/d/1wcOsXPnjJZSdNDju826LeR67f39b8z0s/view?usp=sharing"
             }
             target="blank"
             download
