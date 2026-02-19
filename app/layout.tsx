a5d094910a718b0656e8867737d3ab030a8038fd
ejim11 <favourejim56@gmail.com>
2026-02-19 22:24:44 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import React from "react";
 import { NextFontWithVariable } from "next/dist/compiled/@next/font";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
